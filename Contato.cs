b70106643f2e98fe3529eabd3a9d7123e6894e2a
carl10202 <carloshenriferreira20@gmail.com>
2024-04-25 15:24:09 -0300

    Criado o método ToString
    Agora temos o método ToString pronto

--- Contato.cs
+++ Contato.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,64 @@ namespace ListaDeContatosT6
 {
     internal class Contato
     {
+        //Declaração de variaveis.
+        private string _id;
+        private string sobrenome;
+        private string telefone;
+
+        //Propiedades. São os métodos "get" e "set"
+        //get = leitura
+        //set = definição
+        //é posssivel ser somente leitura, ou somente definição...
+        public string Nome
+        {
+            get
+            {
+                return Nome;
+
+            }
+            set
+            {
+                Nome = value;
+            }
+
+        }
+        public string Sobrenome
+        {
+            get
+            {
+                return Sobrenome;
+
+            }
+            set
+            {
+                sobrenome = value;
+            }
+        { set}
+          public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+            set
+            {
+                if (value, Length == 13)
+
+         {
+                    telefone = value;
+                }
+             else
+                {
+                    telefone = "(00) 00000-0000";
+                }
+            }
+            public Contato ()
+           {
+            Nome = "Cláudio";
+            Sobrenome 
+           }
+
+        }
     }
 }
